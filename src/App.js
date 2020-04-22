d01bbde7fc07e16199779b50fc8ef98a83741857
小林優一 <y.k94cofitec179@gmail.com>
2020-04-22 17:39:00 +0900

    リスト表示まで

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <form clasName="App-form">
+        <form className="App-form">
           <div>
             <input id="title" placeholder="title" />
             <textarea id="description" placeholder="description" />
